5d776768ce4b31525641ae523c43e319e81ba8bf
Lorenzo Fontoura <lorenzo@nioel.com>
2021-01-14 12:16:45 +1000

    create app reducer

---
+++ app/src/store/app/actions/setWindowDimensions.js
@@ -0,0 +1,21 @@
+﻿﻿export const ActionType = "APP_SET_WINDOW_DIMENSIONS";
+
+export const createAction = (width, height) => ({
+  type: ActionType,
+  dimensions: {
+    width,
+    height,
+  },
+});
+
+export const Reducer = (state, action) => {
+  return {
+    ...state,
+    window: {
+      width: action.dimensions.width,
+      height: action.dimensions.height,
+    },
+  }
+};
+
+export default { ActionType, Reducer, createAction };
---
+++ app/src/store/app/index.js
@@ -0,0 +1,16 @@
+﻿﻿import ReducerHandler from "../handler";
+import setWindowDimensions from "./actions/setWindowDimensions";
+
+const initialState = {
+  window: {
+    width: 0,
+    height: 0,
+  },
+};
+
+export default ReducerHandler(
+  initialState,
+  [
+    setWindowDimensions,
+  ]
+);
