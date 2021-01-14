9ecf18293f8a6a8c113fb189f6ae9b3b6684a30e
Lorenzo Fontoura <lorenzo@nioel.com>
2021-01-14 12:15:57 +1000

    create new redux store

---
+++ app/src/store/handler.js
@@ -0,0 +1,15 @@
+﻿const handlerFactory = (initialState, reducerModules) => {
+  const reducers = [];
+  reducerModules.forEach(module => {
+    reducers[module.ActionType] = module.Reducer;
+  });
+  
+  return (state = initialState, action) => {
+    const reducer = reducers[action.type];
+    if (reducer == null)
+      return state;
+    return reducer(state, action);
+  };
+};
+
+export default handlerFactory;
