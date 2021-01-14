99b3b719ee41a59d096a471cfed15a35ed51d951
Lorenzo Fontoura <lorenzo@nioel.com>
2021-01-14 16:54:53 +1000

    update store for navigation and claim actions

---
+++ app/src/store/app/actions/selectClaim.js
@@ -0,0 +1,18 @@
+﻿﻿﻿export const ActionType = "APP_SELECT_CLAIM";
+
+export const createAction = (claim) => ({
+  type: ActionType,
+  claim,
+});
+
+export const Reducer = (state, action) => {
+  return {
+    ...state,
+    claims: {
+      ...state.claims,
+      selected: action.claim,
+    },
+  };
+};
+
+export default { ActionType, Reducer, createAction };
--- app/src/store/app/index.js
+++ app/src/store/app/index.js
@@ -1,7 +1,16 @@
-﻿﻿import ReducerHandler from "../handler";
+﻿import selectClaim from "./actions/selectClaim";
+
+﻿import ReducerHandler from "../handler";
 import setWindowDimensions from "./actions/setWindowDimensions";
+import setScene from "./actions/setNavigation";
 
 const initialState = {
+  navigation: {
+    canGoBack: false,
+  },
+  claims: {
+    selected: null,
+  },
   window: {
     width: 0,
     height: 0,
@@ -12,5 +21,7 @@ export default ReducerHandler(
   initialState,
   [
     setWindowDimensions,
+    setScene,
+    selectClaim,
   ]
 );
