61f762371704375431f53292116a4adedd2c7b35
Lorenzo Fontoura <lorenzo@nioel.com>
2021-01-15 11:53:52 +1000

    add selectVendor store action

---
+++ app/src/store/app/actions/selectVendor.js
@@ -0,0 +1,18 @@
+﻿﻿﻿﻿export const ActionType = "APP_SELECT_VENDOR";
+
+export const createAction = (vendor) => ({
+  type: ActionType,
+  vendor,
+});
+
+export const Reducer = (state, action) => {
+  return {
+    ...state,
+    vendors: {
+      ...state.vendors,
+      selected: action.vendor,
+    },
+  };
+};
+
+export default { ActionType, Reducer, createAction };
