bee286f57a13a90eeed9683ab64cee291b4e753d
Lorenzo Fontoura <lorenzo@nioel.com>
2021-01-15 12:14:12 +1000

    update vendor selection scene

--- app/src/components/scenes/vendors/index.js
+++ app/src/components/scenes/vendors/index.js
@@ -1,45 +1,105 @@
-import React from "react";
+import React, {useEffect} from "react";
 import PropTypes from "prop-types";
 import {
-  StyleSheet, Text, View, StatusBar,
+  Text, View, StatusBar, FlatList, TouchableOpacity,
 } from "react-native";
-import { colors } from "../../../styles/theme";
+import {bindActionCreators} from "redux";
+import {
+  createAction as createSetNavigationAction
+} from "../../../store/app/actions/setNavigation";
+import {
+  createAction as createSelectVendorAction
+} from "../../../store/app/actions/selectVendor";
+import {
+  createAction as createLoadAssetAction
+} from "../../../store/app/actions/loadAsset";
+import {connect} from "react-redux";
+import assets from "../../../lib/assets";
+import styles from "../../../styles";
 
-const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: colors.lightGrayPurple,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 60,
-  },
-});
+const VendorListItem = ({ onPress, window, name, url }) => (
+  <TouchableOpacity style={styles.list.itemWrapper(window)} onPress={onPress}>
+    <Text style={styles.list.itemName}>{name}</Text>
+    <Text style={{ color: "#707070" }}>{url}</Text>
+  </TouchableOpacity>
+);
 
-const Vendors = ({ route, navigation }) => {
+const VendorSelector = ({
+ navigation,
+ app,
+ selectVendor,
+ setNavigation,
+ loadAsset,
+}) => {
+  useEffect(() => {
+    (async () => {
+      const vendors = await assets.fetch(assets.Type.Vendors);
+      loadAsset(assets.Type.Vendors, vendors);
+    })();
+  }, []);
+
+  useEffect(() => {
+    navigation.addListener("focus", () => {
+      setNavigation({
+        canGoBack: false,
+      });
+    });
+  }, [navigation]);
+
+  if (app.assets.vendors == null)
+    return (
+      <View style={styles.list.root}>
+        <Text>Loading...</Text>
+      </View>
+    );
+
   return (
-    <View style={styles.root}>
-      <StatusBar barStyle="light-content" />
-      <Text style={styles.title}>3rd Parties</Text>
+    <View style={styles.list.root}>
+      <StatusBar barStyle="light-content"/>
+      <FlatList
+        data={app.assets.vendors}
+        style={{width: app.window.width}}
+        renderItem={({item}) =>
+          <VendorListItem
+            key={item.url}
+            {...item}
+            window={app.window}
+            onPress={() => {
+              selectVendor(item);
+              setNavigation({
+                canGoBack: true,
+              });
+              navigation.navigate("Vendor");
+            }}
+          />
+        }
+      />
     </View>
   );
 };
 
-Vendors.propTypes = {
-  route: PropTypes.shape({
-    params: PropTypes.shape({ from: PropTypes.string }),
-  }),
+VendorSelector.propTypes = {
   navigation: PropTypes.shape({
-    goBack: PropTypes.func,
+    navigate: PropTypes.func,
   }),
 };
 
-Vendors.defaultProps = {
-  route: { params: { from: "" } },
-  navigation: { goBack: () => null },
+VendorSelector.defaultProps = {
+  navigation: { navigate: () => null },
 };
 
-export default Vendors;
+const mapStateToProps = (state) => ({
+  user: state.user,
+  app: state.app,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setNavigation: bindActionCreators(createSetNavigationAction, dispatch),
+  selectVendor: bindActionCreators(createSelectVendorAction, dispatch),
+  loadAsset: bindActionCreators(createLoadAssetAction, dispatch),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(VendorSelector);
