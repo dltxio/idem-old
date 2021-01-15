ce9998fb94133b8bbcf14aa5a499c4b579e45930
Lorenzo Fontoura <lorenzo@nioel.com>
2021-01-15 10:49:49 +1000

    add assets object to store

---
+++ app/src/store/app/actions/loadAsset.js
@@ -0,0 +1,21 @@
+﻿﻿﻿﻿export const ActionType = "APP_LOAD_ASSET";
+
+export const createAction = (assetType, assetValue) => ({
+  type: ActionType,
+  asset: {
+    type: assetType,
+    value: assetValue,
+  },
+});
+
+export const Reducer = (state, { asset }) => {
+  return {
+    ...state,
+    assets: {
+      ...state.assets,
+      [asset.type]: asset.value,
+    },
+  };
+};
+
+export default { ActionType, Reducer, createAction };
