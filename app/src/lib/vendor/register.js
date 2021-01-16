18024afc974d4ed55ac152214db73482a0781d4d
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-01-16 22:15:39 +1000

    Change to API

--- app/src/lib/vendor/register.js
+++ app/src/lib/vendor/register.js
@@ -1,7 +1,27 @@
 ﻿import { Linking } from "react-native";
 
 const register = async (vendor) => {
-  Linking.openURL(vendor.url);
+  const endpoint = "https://testapi.getpaidinbitcoin.com.au/user";
+  const data = JSON.stringify({
+    firstName: "Test",
+    secondName: "User",
+    email: "lucas3@lucascullen.com",
+    password: "Test1234!",
+  });
+
+  const config = {
+    method: "post",
+    url: "https://testapi.getpaidinbitcoin.com.au/user",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
+
+  const response = await axios.post(endpoint, data);
+  if (response.data == null)
+    throw new Error(`Could not post data to endpoint "${endpoint}"`);
+  return response.data;
 };
 
 export default register;
