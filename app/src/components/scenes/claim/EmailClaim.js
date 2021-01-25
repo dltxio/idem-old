9abbba2ed911f5a074d4e2de13752ff1d1a66a4c
Alex Xiang <xiangzhe0812@gmail.com>
2021-01-25 13:23:20 +1000

    ID-18

---
+++ app/src/components/scenes/claim/EmailClaim.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import AsyncStorage from "@react-native-community/async-storage";
+import isEmail from "validator/lib/isEmail";
+import { Text, View, TextInput, Button } from "react-native";
+import styles from "../../../styles";
+
+const EmailClaim = ({ window }) => {
+  const [email, setEmail] = useState();
+  const [error, setError] = useState();
+
+  return (
+    <>
+      <TextInput
+        style={{
+          ...styles.claim.input,
+          width: Math.floor(window.width),
+        }}
+        onChangeText={(value) => {
+          setEmail(value);
+        }}
+        onBlur={async () => {
+          if (isEmail(email)) {
+            setError("");
+            try {
+              await AsyncStorage.setItem("@email", email);
+              //console.log(await AsyncStorage.getItem("@email")); get item email from storage for test
+            } catch (error) {
+              setError(error);
+            }
+          } else {
+            setError("Please enter a valid email");
+          }
+        }}
+      />
+      <Text style={styles.claim.errorMessage}>{error}</Text>
+    </>
+  );
+};
+
+export default EmailClaim;
