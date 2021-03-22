df3c9fd3876a43a27d4c5a05a953d531bfbfaba9
Lorenzo Fontoura <lorenzo@nioel.com>
2021-03-22 13:45:52 +1000

    add basic auth to sms routes

--- server/src/endpoint/sms/index.js
+++ server/src/endpoint/sms/index.js
@@ -3,29 +3,31 @@ const router = express.Router();
 const schema = require("./schema");
 const message = require("./message");
 const { log } = require("../../logger")("/api/sms");
+const config = require("../../../config.json");
+const basicAuth = require("express-basic-auth")(config.basicAuth);
 
-router.put("/sms", async (request, response) => {
+const requestCode = async (request, response) => {
   const { error, value } = schema.put.validate(request.body);
-  
+
   const sendError = (code, error) => {
     response.status(code);
     response.send({ error });
   };
-  
+
   if (error != null)
     return sendError(400, error);
-  
+
   try {
     const data = await message.sendVerification(value.number);
     log(`received SMS request for ${value.number} -- code:`, data.code);
   } catch(smsError) {
     return sendError(500, smsError);
   }
-  
-  response.send({ success: true, error: null });
-});
 
-router.post("/sms", async (request, response) => {
+  response.send({ success: true, error: null });
+};
+
+const verifyCode = async (request, response) => {
   const { error, value } = schema.post.validate(request.body);
 
   const sendError = (code, error) => {
@@ -39,13 +41,17 @@ router.post("/sms", async (request, response) => {
   const isValid = message.checkVerification(value.number, value.code);
   log(
     `received SMS verification for ${value.number} with input code ${value.code} [` +
-    (!isValid ? "IN" : "") + "VALID]"  
+    (!isValid ? "IN" : "") + "VALID]"
   );
 
   response.send({
     success: isValid,
     error: isValid ? null : "invalid code"
   });
-});
+};
+
+router.use(basicAuth);
+router.put("/sms", requestCode);
+router.post("/sms", verifyCode);
 
 module.exports = router;
