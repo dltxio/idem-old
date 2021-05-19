84bba14d12c47a30d5bb79db73cfc42297fcd546
Anthony Hinchliff <anthony.hinchliff@gmail.com>
2021-05-19 17:13:39 +1000

    example of using async storage

--- app/screens/profile/index.tsx
+++ app/screens/profile/index.tsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { Avatar } from "react-native-elements";
-
 import AsyncStorage from "@react-native-community/async-storage";
-import claims from "../../../assets/claims.json";
-const emailKey = claims.find(x => x.type === "Email");
-const getEmailFromStorage: () => string | '' =  () => AsyncStorage.getItem(emailKey?.key);
+import claims from "../../assets/claims.json";
+
+const getEmailFromStorage = async (): Promise<string | undefined> => {
+  const emailKey = claims.find((x) => x.type === "Email");
+
+  if (!emailKey) {
+    return undefined;
+  }
+
+  const result = await AsyncStorage.getItem(emailKey.key);
+
+  return result ? result : undefined;
+};
+
+const saveEmailToStorage = async (): Promise<void> => {
+  const emailKey = claims.find((x) => x.type === "Email");
+
+  if (!emailKey) {
+    return undefined;
+  }
+
+  return AsyncStorage.setItem(emailKey.key, "anthony.hinchliff@gmail.com");
+};
 
 const styles = StyleSheet.create({
-
   userRow: {
     alignItems: "center",
     flexDirection: "row",
@@ -20,27 +38,37 @@ const styles = StyleSheet.create({
   userImage: {
     marginRight: 12,
   },
-
 });
-const Profile = () => (
-  
-  <View style={styles.userRow}>
-    <View style={styles.userImage}>
-      <Avatar rounded size="large" source={{ uri: 'https://i.imgur.com/FmeKYGe.jpg' }} />
+const Profile = () => {
+  useEffect(() => {
+    saveEmailToStorage().then(() => {
+      getEmailFromStorage().then((r) => {
+        console.log(r);
+      });
+    });
+  }, []);
+  return (
+    <View style={styles.userRow}>
+      <View style={styles.userImage}>
+        <Avatar
+          rounded
+          size="large"
+          source={{ uri: "https://i.imgur.com/FmeKYGe.jpg" }}
+        />
+      </View>
+      <View>
+        <Text style={{ fontSize: 16 }}>Ronaldo Effertz</Text>
+        <Text
+          style={{
+            color: "gray",
+            fontSize: 16,
+          }}
+        >
+          ronaldo.effertz@gmail.com
+        </Text>
+      </View>
     </View>
-    <View>
-      <Text style={{ fontSize: 16 }}>Ronaldo Effertz</Text>
-      <Text
-        style={{
-          color: "gray",
-          fontSize: 16,
-        }}
-      >
-        {getEmailFromStorage()}
-        ronaldo.effertz@gmail.com
-      </Text>
-    </View>
-  </View>
-);
+  );
+};
 
 export default Profile;
