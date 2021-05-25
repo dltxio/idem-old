28ba2f061f94b2f43f138eb3b4172cf87fa02e77
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-05-25 17:59:55 +1000

    Chore: Working o365

---
+++ server/email.js
@@ -0,0 +1,13 @@
+// var nodeoutlook = require("nodejs-nodemailer-outlook");
+// nodeoutlook.sendEmail({
+//   auth: {
+//     user: "admin@bitcoinbrisbane.com.au",
+//     pass: ""
+//   },
+//   from: "admin@bitcoinbrisbane.com.au",
+//   to: "lucas@lucascullen.com",
+//   subject: "Hey you, awesome!",
+//   text: "This is text version!",
+//   onError: e => console.log(e),
+//   onSuccess: i => console.log(i)
+// });
