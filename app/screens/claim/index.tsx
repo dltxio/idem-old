d2693fd466b3af9f7eeb348bb8a03aabeabaa104
Dinesh Sharma <sharma.dinesh907@gmail.com>
2021-05-28 16:58:35 +1000

    remove conflict

--- app/screens/claim/index.tsx
+++ app/screens/claim/index.tsx
@@ -72,7 +72,7 @@ const Claim = () => {
       // Ask the user for the permission to access the camera
       const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
       if (permissionResult.granted === false) {
-        alert("You've refused to allow this appp to access your camera!");
+        alert("You've refused to allow this app to access your camera!");
         return;
       }
       const result = await ImagePicker.launchCameraAsync();
