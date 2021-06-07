14c605f9d211de700e45fb594b8fd5419ca746e1
Lucas Cullen <bitcoinbrisbane@users.noreply.github.com>
2021-06-08 09:20:54 +1000

    Update server/src/endpoint/emails/send-email-verification-email.ts
    Co-authored-by: Lorenzo Fontoura <lorenzo.fontoura@dltx.io>

--- server/src/endpoint/emails/send-email-verification-email.ts
+++ server/src/endpoint/emails/send-email-verification-email.ts
@@ -43,7 +43,6 @@ const sendEmailVerificationEmail: RequestHandler<
     privateKeys: privateKey 
   });
 
-  console.log(signature);
 
   await services.email.sendEmailVerificationEmail({
     to: body.email,
