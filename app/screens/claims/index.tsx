88a8be58976d00c4c879875d692f6538276a5b0f
Dinesh Sharma <sharma.dinesh907@gmail.com>
2021-06-10 09:39:57 +1000

    work on deep linking

--- app/screens/claims/index.tsx
+++ app/screens/claims/index.tsx
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Text,
   View,
   StatusBar,
   TouchableOpacity,
   ViewStyle,
+  Modal,
+  Pressable,
+  StyleSheet,
+  Alert
 } from "react-native";
 import styles from "../../styles";
 import { useNavigation } from "@react-navigation/core";
@@ -13,14 +17,16 @@ import { observer } from "mobx-react-lite";
 import { IClaim } from "../../store/assetStore";
 import { colors } from "../../styles/theme";
 import Profile from "../profile";
+import * as Linking from "expo-linking";
 
-
+// const  prefix = Linking.makeUrl("/");
 type ClaimsListItemProps = {
   onPress: () => void;
   item: IClaim;
 };
 
 const ClaimListItem = ({ onPress, item }: ClaimsListItemProps) => (
+  
   <TouchableOpacity
     style={styles.list.itemWrapper(styles.layout.window)}
     onPress={onPress}
@@ -63,9 +69,41 @@ const ClaimSelector = () => {
   const navigation = useNavigation();
   const rootStore = useRootStore();
   const assetStore = rootStore.Assets;
+  const [modalVisible, setModalVisible] = useState(false);
+  const [data, setData] = useState<any | null>(null);
+
+  // const linking = {
+  //   prefixes: [prefix],
+  // };
+  // console.log('linking========>S', linking);
+  const handleDeepLink = (event: any) => {
+    console.log('event', event);
+    const data = Linking.parse(event.url);
+    console.log('data', data)
+    setData(data);
+  }
 
   useEffect(() => {
     assetStore.loadClaims();
+    // const  getInitialUrl = async () => {
+    //   const initialUrl = await Linking.getInitialURL();
+    //   console.log('initialUrl', initialUrl);
+    //   if(initialUrl) setData(Linking.parse(initialUrl));
+    // }
+    var NativeLinking=require("../../node_modules/react-native/Libraries/Linking/NativeLinking").default
+    NativeLinking.getInitialURL().then((url: any) => {
+      console.log('Initial url is: ' + url);
+      setData(Linking.parse(url))
+    }).catch((err:any) => {console.error('An error occurred', err)});
+    Linking .addEventListener("url", handleDeepLink);
+    if(!data) {
+      // getInitialUrl();
+      setModalVisible(true);
+    } else {
+    }
+    return () => {
+      Linking.removeEventListener("url", handleDeepLink);
+    };
   }, []);
 
   if (assetStore.claims == null)
@@ -76,27 +114,112 @@ const ClaimSelector = () => {
     );
 
   return (
-    <View style={styles.list.root as ViewStyle}>
-      <StatusBar barStyle="light-content" />
-      {assetStore.claims[0] && <Profile fullName={assetStore.claims.find((c => c.key === "0x02"))!} emailAddress={assetStore.claims.find((c => c.key === "0x03"))!} />}
-      <View style={{ flex: 1, width: "100%" }}>
-        {assetStore.claims.map((item, index) => {
-          return (
-            <>
-            <ClaimListItem
-              key={index}
-              item={item}
-              onPress={() => {
-                rootStore.Assets.setClaimKey(item.key);
-                navigation.navigate("Claim");
-              }}
-            />
-            </>
-          );
-        })}
+    <>
+      <View style={styles.list.root as ViewStyle}>
+        <StatusBar barStyle="light-content" />
+        {assetStore.claims[0] && <Profile fullName={assetStore.claims.find((c => c.key === "0x02"))!} emailAddress={assetStore.claims.find((c => c.key === "0x03"))!} />}
+        <View style={{ flex: 1, width: "100%" }}>
+          {assetStore.claims.map((item, index) => {
+            return (
+              <>
+              <ClaimListItem
+                key={index}
+                item={item}
+                onPress={() => {
+                  rootStore.Assets.setClaimKey(item.key);
+                  navigation.navigate("Claim");
+                }}
+              />
+              </>
+            );
+          })}
+        </View>
       </View>
-    </View>
+      {data ? 
+      <View style={styless.centeredView}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+            setModalVisible(!modalVisible);
+          }}
+        >
+        <View style={styless.centeredView}>
+          <View style={styless.modalView}>
+            <Text style={styless.modalText}>Host Name:- {data.hostname}</Text>
+            <Text style={styless.modalText}>{JSON.stringify(data)}</Text>
+            <Pressable
+              style={[styless.button, styless.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styless.textStyle}>Ok</Text>
+            </Pressable>
+            <Pressable
+              style={[styless.button, styless.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styless.textStyle}>Cancel</Text>
+            </Pressable>
+          </View>
+        </View>
+        </Modal>
+      {/* <Pressable
+        style={[styless.button, styless.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styless.textStyle}>Show Modal</Text>
+      </Pressable> */}
+      </View>
+      : null}
+    </>
   );
 };
 
 export default observer(ClaimSelector);
+
+const styless = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    width: 350,
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
+});
--- app/screens/qrcode/index.tsx
+++ app/screens/qrcode/index.tsx
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IClaim } from "../../store/assetStore";
 import SvgQRCode from 'react-native-qrcode-svg';
 import { observer } from "mobx-react-lite";
+import * as Linking from "expo-linking";
+import { Text } from "react-native";
+
+const  prefix = Linking.makeUrl("/");
 
 const Qrcode : React.FC<{claim: IClaim}> = ({ claim }) => {
+
+  const [data, setData] = useState<any | null>(null);
+
+  const linking = {
+    prefixes: [prefix],
+    config: {
+      screens: {
+        Settings: "Settings"
+      },
+    },
+  };
+
+  // console.log('linking=====>', linking);
+
+  const handleDeepLink = (event: any) => {
+    console.log('event', event);
+    const data = Linking.parse(event.url);
+    setData(data);
+  }
+
+  useEffect(() => {
+     const  getInitialUrl = async () => {
+      const initialUrl = await Linking.getInitialURL();
+      if(initialUrl) setData(Linking.parse(initialUrl));
+    }
+    Linking .addEventListener("url", handleDeepLink);
+    if(!data) {
+      getInitialUrl();
+    }
+    return () => {
+      // Linking.removeEventListener("url");
+    };
+  }, [])
   return (
-    <SvgQRCode value={`https://google.com/${claim.value}`} />
+    <>
+    <Text>{data ? JSON.stringify(data): 'Hello'}</Text>
+    <SvgQRCode value={'exp://192.168.1.141:19000'} />
+    </>
   );
 };
 
