0d0b7952425f82be7fb402ae9700c7e44ffe9238
Dinesh Sharma <sharma.dinesh907@gmail.com>
2021-06-15 20:15:13 +1000

    call fake endpoint

--- app/apis/gpib.ts
+++ app/apis/gpib.ts
@@ -1,7 +1,5 @@
-import config from "../../app/config.dev.json";
 import axios from "axios";
 const open = axios.create({
-  baseURL: config.REACT_APP_GPIB_URL.endpoint,
   headers: {
     "Cache-Control": "no-store",
   },
