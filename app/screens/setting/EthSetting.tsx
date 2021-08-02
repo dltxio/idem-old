66d07702bb171151aee4c6e8f4bb47335497f025
Alex Xiang <xiangzhe0812@gmail.com>
2021-08-02 16:57:22 +1000

    create new eth keypair

--- app/screens/setting/EthSetting.tsx
+++ app/screens/setting/EthSetting.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, ViewStyle, TextInput } from "react-native";
 import styles from "../../styles";
 import Button from "../../components/Button";
 import { GetEthKeypair } from "../../helpers/setting/EthKeypair";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ethers } from "ethers";
+import * as Random from "expo-random";
 
 const EthSetting = () => {
   const [error, setError] = useState<string | undefined>(undefined);
@@ -26,6 +28,24 @@ const EthSetting = () => {
       setError("Please enter a mnemonic key");
     }
   };
+
+  const onCreateEthKeypairPress = async () => {
+    try {
+      const newPhrase = await await Random.getRandomBytesAsync(16);
+      const mnemonic = ethers.utils.entropyToMnemonic(newPhrase);
+      const response = await GetEthKeypair(mnemonic);
+      if (response) {
+        await AsyncStorage.setItem("eth-keypair", JSON.stringify(response));
+        setMessage(
+          `Your keypair has been stored in your device, here is your address : "${response.address}" and mnemonic: "${mnemonic}", please save it to revocery your account`,
+        );
+      }
+    } catch (error) {
+      console.error(error.message);
+      setError(error.message);
+    }
+  };
+
   return (
     <View style={{ flex: 1 }}>
       <TextInput
@@ -47,9 +67,14 @@ const EthSetting = () => {
       {!!error && <Text style={styles.claim.errorMessage}>{error}</Text>}
       {message && <Text>{message}</Text>}
       <Button
-        title="Get ETH Keypair"
-        style={styles.claim.verifyButton as ViewStyle}
+        title="Get ETH Keypair from Mnemonic"
+        style={styles.claim.libraryUploadButton as ViewStyle}
         onPress={onGetKeypairPress}
+      />
+      <Button
+        title="Create ETH keypair"
+        style={styles.claim.verifyButton as ViewStyle}
+        onPress={onCreateEthKeypairPress}
       />
     </View>
   );
