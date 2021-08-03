928b82e564f511a9d330fd0540746b2814b1d600
Alex Xiang <xiangzhe0812@gmail.com>
2021-08-03 18:36:58 +1000

    styling onboard page add formik

--- app/screens/onboard/index.tsx
+++ app/screens/onboard/index.tsx
@@ -1,8 +1,26 @@
-import React from "react";
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import React, { useState, useRef } from "react";
+import { View, Text, TextInput, Button, TouchableOpacity } from "react-native";
 import PagerView from "react-native-pager-view";
+import styles from "../../styles";
+import DatePick from "../../components/DatePick";
+import moment from "moment";
+import { Formik, ErrorMessage } from "formik";
 
+type OnboardProp = {
+  name: string;
+  email: string;
+  dob: string;
+};
 const Onboard = () => {
+  const [showDate, setShowDate] = useState(false);
+  const [data, setDate] = useState<string | undefined>(undefined);
+
+  const validate = Yup.object().shape({
+    name: Yup.string().required("Name is required"),
+    emial: Yup.string().email(),
+    dof: Yup.string().required("DOB is required"),
+  });
+
   return (
     <View
       style={{
@@ -10,54 +28,65 @@ const Onboard = () => {
         flexGrow: 1,
       }}
     >
-      <PagerView style={styles.viewPager} initialPage={0}>
-        <View
-          style={{
-            paddingTop: 50,
-            height: 400,
-            backgroundColor: "#061121",
-          }}
+      <PagerView style={styles.baseStyle.viewPager} initialPage={0}>
+        <Formik
+          initialValues={{ name: "", email: "", date: "" }}
+          onSubmit={() => console.log("submit")}
+          validationSchema={validate}
         >
-          <TextInput
-            keyboardType="default"
-            placeholder="Please enter your first and last names"
-          />
-          <TextInput
-            keyboardType="email-address"
-            placeholder="Please enter your email address..."
-          />
-        </View>
-        <View
-          style={{
-            paddingTop: 50,
-            height: 400,
-
-            backgroundColor: "green",
-          }}
-        >
-          <TextInput
-            keyboardType="default"
-            placeholder="Please enter your first and last names"
-          />
-          <TextInput
-            keyboardType="email-address"
-            placeholder="Please enter your email address..."
-          />
-        </View>
+          {({ handleSubmit, values }) => (
+            <View style={styles.baseStyle.viewPage} key="1">
+              <View style={{ alignItems: "center", paddingBottom: 20 }}>
+                <Text style={styles.baseStyle.baseTitle}>On boarding</Text>
+              </View>
+              <Text style={styles.baseStyle.baseText}>Full Name</Text>
+              <TextInput
+                style={styles.baseStyle.baseTextInput}
+                keyboardType="default"
+                placeholder="Please enter your first and last names"
+                placeholderTextColor="gray"
+                value={values.name}
+              />
+              <ErrorMessage />
+              <Text style={styles.baseStyle.baseText}>Email</Text>
+              <TextInput
+                style={styles.baseStyle.baseTextInput}
+                keyboardType="email-address"
+                placeholder="Please enter your email address..."
+                placeholderTextColor="gray"
+                value={values.email}
+              />
+              <Text style={styles.baseStyle.baseText}>DOB</Text>
+              <TouchableOpacity
+                onPress={() => setShowDate(!showDate)}
+                style={styles.baseStyle.dataPick}
+              >
+                <Text style={styles.baseStyle.dataText}>{`${
+                  moment(data).format("DD/MM/YYYY") ||
+                  "Please enter the specific date..."
+                }`}</Text>
+              </TouchableOpacity>
+              <DatePick
+                show={showDate}
+                handleCloseDate={() => {
+                  setShowDate(false);
+                }}
+                handleDateChange={(value: Date) => {
+                  setDate(value.toUTCString());
+                  setShowDate(false);
+                }}
+              />
+              <Button
+                title="Submit"
+                style={styles.baseStyle.baseButton}
+                onPress={handleSubmit}
+              />
+            </View>
+          )}
+        </Formik>
       </PagerView>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  viewPager: {
-    flex: 1,
-    flexGrow: 1,
-  },
-  page: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
 
 export default Onboard;
---
+++ app/styles/baseStyle.ts
@@ -0,0 +1,50 @@
+export default {
+  viewPager: {
+    flex: 1,
+    flexGrow: 1,
+  },
+  viewPage: {
+    paddingTop: 200,
+    height: 400,
+    backgroundColor: "#061121",
+  },
+  page: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  baseTextInput: {
+    padding: 10,
+    color: "white",
+    margin: 20,
+    borderColor: "#007bff",
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  dataPick: {
+    padding: 10,
+    color: "white",
+    margin: 20,
+    borderColor: "#007bff",
+    borderWidth: 1,
+    borderRadius: 5,
+    height: 40,
+  },
+  dataText: {
+    color: "white",
+    fontSize: 15,
+    fontWeight: "500",
+  },
+  baseText: {
+    color: "white",
+    fontSize: 15,
+    fontWeight: "500",
+    marginLeft: 20,
+  },
+  baseTitle: { color: "white", fontSize: 20, fontWeight: "500" },
+  baseButton: {
+    paddingTop: 20,
+    borderWidth: 1,
+    borderRadius: 5,
+    color: "#007bff",
+  },
+};
