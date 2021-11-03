1d3c810dee164c1a854c354ff56d4381bae0c17b
Robert Bartlett <amagon270@gmail.com>
2021-11-03 16:31:14 +1000

    added other fields to claim

--- server-interfaces/src/claims.d.ts
+++ server-interfaces/src/claims.d.ts
@@ -1,16 +1,22 @@
 declare namespace server {
-  type Claim = {
+  type URI = string;
+
+  interface Claim {
     "@context": string[];
-    id?: string;
+    id?: URI;
     key: string;
-    type: string[];
-    credentialSubject: CredentialSubject;
-    proof: Proof;
-    issuer: string;
-    issuanceDate: Date;
-    expirationDate?: Date;
     title: string;
     description?: string;
+    type: URI[];
+    credentialSubject: CredentialSubject;
+    proof: Proof;
+    issuer: URI;
+    issuanceDate: Date;
+    expirationDate?: Date;
+    credentialSchema?: CredentialSchema;
+    refreshService?: RefreshService;
+    termsOfUse?: TermsOfUse;
+    evidence?: Evidence[];
   }
 
   type ClaimValidated = {
@@ -28,17 +34,44 @@ declare namespace server {
   };
 
   interface CredentialSubject {
-    id?: string;
-    name: string;
+    id?: URI;
+    name: URI;
     value: any;
   }
 
   interface Proof {
-    type: string;
+    type: URI;
   }
   
   interface CredentialStatus {
-    id: string;
-    type: string
+    id?: URI;
+    type: URI
+  }
+
+  interface CredentialSchema {
+    id?: URI;
+    type: URI;
+  }
+
+  interface RefreshService {
+    id?: URI;
+    type: URI;
+  }
+
+  interface TermsOfUse {
+    id?: URI;
+    type: URI;
+    obligation?: any;
+    prohibition?: any;
+    permission?: any;
+  }
+
+  interface Evidence {
+    id?: URI;
+    type: URI[];
+    verifier: URI;
+    evidenceDocument: string;
+    subjectPresence: string;
+    documentPresence: string;
   }
 }
