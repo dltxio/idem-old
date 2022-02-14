efd4088e14285f5efd4dfde610945187999b4ade
Preston Harrison <preston@prestonharrison.com>
2022-02-14 13:40:54 +1000

    fix

--- app/App.tsx
+++ app/App.tsx
@@ -8,18 +8,25 @@ export type Screens = "Info" | "ClaimRequest";
 
 const App = () => {
   const [screen, setScreen] = useState<Screens>("Info");
-  const [claimRequestData, setClaimRequestData] = useState<any>({});
+  const [claimRequestData, setClaimRequestData] = useState<{
+    claims: string, // A json stringified array of numbers
+    callback: string, // base 64 encoded url
+    nonce: string
+  } | null>(null);
   Linking.addEventListener('url', (event) => {
     const data = Linking.parse(event.url);
-    setClaimRequestData(data.queryParams);
-    setScreen("ClaimRequest");
+    if (!data.queryParams.claims || !data.queryParams.nonce || !data.queryParams.callback) return;
+    if (data.queryParams) {
+      setClaimRequestData(data.queryParams);
+      setScreen("ClaimRequest");
+    }
   })
 
   return (
     <View style={{ flex: 1 }}>
       {/* <Text style={{ margin: 50 }}>{JSON.stringify(claimRequestData)}</Text> */}
       {screen === "Info" && <Info />}
-      {screen === "ClaimRequest" && <ClaimRequest setScreen={setScreen} claimData={claimRequestData} />}
+      {screen === "ClaimRequest" && claimRequestData && <ClaimRequest setScreen={setScreen} claimData={claimRequestData} />}
     </View>
   );
 };
